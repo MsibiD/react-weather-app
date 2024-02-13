34b702a25dec51fbd0d799cf37d9ea86d22625de
MsibiD <156788159+MsibiD@users.noreply.github.com>
2024-02-13 14:24:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default function App() {
           </a>{" "}
           and{" "}
           <a
-            href="https://suspicious-beaver-111c4d.netlify.com/"
+            href="https://storied-fenglisu-832e07.netlify.app//"
             target="_blank"
             rel="noopener noreferrer"
           >
